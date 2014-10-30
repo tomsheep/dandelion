0ba26cd1b1de15c10ec5488d9937055f2340d8f5
garfieldull <kancycherish@gmail.com>
2014-10-30 19:10:12 +0800

    homework2 pset

--- tutorials/web02/src/config.py
+++ tutorials/web02/src/config.py
@@ -1,2 +1,12 @@
 SQLITE_DB = 'blog.db'
 DB_CREATE_SQL = open('schema.sql').read()
+
+MySQL_DB = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'pw': 'admin@dev',
+    'port': 3306,
+    'db': 'blog'
+}
+
+
--- tutorials/web02/src/schema.sql
+++ tutorials/web02/src/schema.sql
@@ -1,7 +1,15 @@
-CREATE TABLE entries (
+/*CREATE TABLE entries (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     title TEXT,
     subtitle TEXT,
     content TEXT,
     posted_on DATETIME
-);
+);*/
+CREATE TABLE IF NOT EXISTS entries (
+	id INT(10) unsigned NOT NULL auto_increment,
+	title VARCHAR(30) NOT NULL default '',
+	subtitle VARCHAR(30) NOT NULL default '',
+	content TEXT,
+	posted_on timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
